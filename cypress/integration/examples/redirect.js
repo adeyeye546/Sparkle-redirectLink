6404bc59494c7bae8b60f82d39b1529bcd2f0252
adeyeye546 <adeyeye546@gmail.com>
2023-07-24 00:54:35 +0100

    first commit

---
+++ cypress/integration/examples/redirect.js
@@ -0,0 +1,19 @@
+describe('Redirection Link Test', () => {
+    it('should redirect to the correct page', () => {
+      // Define the URL of the page with the redirection link
+      const initialPageUrl = 'https://www.sparkle.com';
+  
+      // Define the expected target URL after redirection
+      const expectedTargetUrl = 'http://the-internet.herokuapp.com/status_codes';
+  
+      // Visit the initial page with the redirection link
+      cy.visit(initialPageUrl);
+  
+      // Click the redirection link
+      cy.get('a#redirect-link').click();
+  
+      // Assert that the current URL matches the expected target URL after redirection
+      cy.url().should('eq', expectedTargetUrl);
+    });
+  });
+  
